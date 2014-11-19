f33773203f436dc653dc6d0d0f561af0064a8135
Ricardo <ricardoacedo7@gmail.com>
2014-11-19 17:49:22 +0100

    properties

--- Reserva.py
+++ Reserva.py
@@ -1,16 +1,12 @@
 __author__ = 'Ricardo'
 class Reserva():
-    def __init__(self,fecha, socio, instalacion):
-        self.__fecha = fecha
-        self.__socio = socio
-        self.__instalacion = instalacion
+
 
     def get_fecha(self):
         return self.__fecha
 
     def set_fecha(self,fecha):
         self.__fecha = fecha
-
 
     def get_socio(self):
         return self.__socio
@@ -24,10 +20,16 @@ class Reserva():
     def set_instalacion(self,instalacion):
         self.__instalacion = instalacion
 
-    fecha = property(get_fecha(),set_fecha())
-    instalacion = property(get_instalacion(),set_instalacion())
-    socio = property(get_socio(),set_socio())
+    fecha = property(get_fecha,set_fecha)
+    instalacion = property(get_instalacion,set_instalacion)
+    socio = property(get_socio,set_socio)
+
+    def __init__(self,fecha, socio, instalacion):
+        self.__fecha = fecha
+        self.__socio = socio
+        self.__instalacion = instalacion
 
     def __eq__(self, otro):
         return (isinstance(self,otro) and self.__fecha == otro.fecha
         and self.__socio == otro.socio and self.__instalacion == otro.instalacion)
+
