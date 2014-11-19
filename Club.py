9711db88cf42d8f0787524ff1372a0ee8d21bbe3
Ricardo <ricardoacedo7@gmail.com>
2014-11-19 16:31:17 +0100

    añadido club y reserva

---
+++ Club.py
@@ -0,0 +1,5 @@
+__author__ = 'Ricardo'
+
+class Club():
+    def alta_socio(self):
+        return 0
---
+++ Reserva.py
@@ -0,0 +1,33 @@
+__author__ = 'Ricardo'
+class Reserva():
+    def __init__(self,fecha, socio, instalacion):
+        self.__fecha = fecha
+        self.__socio = socio
+        self.__instalacion = instalacion
+
+    def get_fecha(self):
+        return self.__fecha
+
+    def set_fecha(self,fecha):
+        self.__fecha = fecha
+
+
+    def get_socio(self):
+        return self.__socio
+
+    def set_socio(self,socio):
+        self.__socio = socio
+
+    def get_instalacion(self):
+        return self.__instalacion
+
+    def set_instalacion(self,instalacion):
+        self.__instalacion = instalacion
+
+    fecha = property(get_fecha(),set_fecha())
+    instalacion = property(get_instalacion(),set_instalacion())
+    socio = property(get_socio(),set_socio())
+
+    def __eq__(self, otro):
+        return (isinstance(self,otro) and self.__fecha == otro.fecha
+        and self.__socio == otro.socio and self.__instalacion == otro.instalacion)
