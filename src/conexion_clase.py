2d9dbc58611d2ef11db119b2680d7005053ca286
Ricardo <ricardoacedo7@gmail.com>
2014-12-13 13:18:16 +0100

    travis

--- src/conexion_clase.py
+++ src/conexion_clase.py
@@ -1,6 +1,5 @@
 from src import Clase
 import os
-
 __author__ = 'alberto'
 import csv
 import shutil
@@ -16,24 +15,24 @@ class conexion_clase():
         valor = -1
         cont = 0
         encontrado = False
-        while(cont<len(self.__clases) and not(encontrado)):
-            if(self.__clases[cont].get_profesor().DNI==profesor and self.__clases[cont].get_reserva().get_fecha()==fecha):
+        while(cont < len(self.__clases) and not(encontrado)):
+            if(self.__clases[cont].get_profesor().DNI == profesor and self.__clases[cont].get_reserva().get_fecha()==fecha):
                 encontrado = True
-                valor=self.__clases[cont]
+                valor = self.__clases[cont]
             else:
                 cont=cont+1
         return valor
 
-    def guardar_clase(self,clase):
-        f = open(os.path.dirname(__file__)+'/files/clases.csv','a+')
-        texto =''
-        texto+= clase.get_profesor().get_DNI()+'\t'
-        texto+= str(clase.get_reserva().get_fecha().strftime("%d/%m/%y %H:%M"))+'\n'
+    def guardar_clase(self, clase):
+        f = open(os.path.dirname(__file__)+'/files/clases.csv', 'a+')
+        texto = ''
+        texto += clase.get_profesor().get_DNI()+'\t'
+        texto += str(clase.get_reserva().get_fecha().strftime("%d/%m/%y %H:%M"))+'\n'
         f.write(texto)
         f.close()
         self.__clases.append(clase)
 
-    def borrar_clase(self,profesor,fecha):
+    def borrar_clase(self, profesor, fecha):
         tempfile = NamedTemporaryFile(delete=False)
 
         with open(os.path.dirname(__file__)+'/files/clases.csv', 'rb') as csvFile, tempfile:
@@ -41,7 +40,7 @@ class conexion_clase():
             writer = csv.writer(tempfile, delimiter='\t')
 
             for row in reader:
-                if(row[0]!=profesor and row[1]!=str(fecha)):
+                if(row[0] != profesor and row[1] != str(fecha)):
                     writer.writerow(row)
         shutil.move(tempfile.name, os.path.dirname(__file__)+'/files/clases.csv')
         csvFile.close()
@@ -53,7 +52,7 @@ class conexion_clase():
             content = csv.reader(f, delimiter='\t')
             for row in content:
                 fecha = datetime.strptime(row[1], "%d/%m/%y %H:%M")
-                clase = Clase(self.sacar_profesor(row[0]),self.sacar_reserva(fecha))
+                clase = Clase(self.sacar_profesor(row[0]), self.sacar_reserva(fecha))
                 clases.append(clase)
         f.close()
         return clases
